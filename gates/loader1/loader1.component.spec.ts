ca5b1e36b6a5a2614ad8d367ada20b8633390ba9
tarxemo <tarxemo@gmail.com>
2024-12-12 20:12:09 +0300

    loader

---
+++ gates/loader1/loader1.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Loader1Component } from './loader1.component';
+
+describe('Loader1Component', () => {
+  let component: Loader1Component;
+  let fixture: ComponentFixture<Loader1Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [Loader1Component]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Loader1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ gates/loader1/loader1.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader1',
+  standalone: false,
+  templateUrl: './loader1.component.html',
+  styleUrls: ['./loader1.component.scss'],
+})
+export class Loader1Component {}
